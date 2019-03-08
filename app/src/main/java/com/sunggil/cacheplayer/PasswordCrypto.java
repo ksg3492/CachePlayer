abcc860fbf506c5fca3020a2479154d73e7bc0e5
kimsunggil <ksg3492@2beone.net>
2019-03-08 16:06:08 +0900

    SecureRandom sr = SecureRandom.getInstance("SHA1PRNG", "Crypto"); -> SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");

--- app/src/main/java/com/sunggil/cacheplayer/PasswordCrypto.java
+++ app/src/main/java/com/sunggil/cacheplayer/PasswordCrypto.java
@@ -56,7 +56,8 @@ public class PasswordCrypto {
 
 	private static byte[] getRawKey(byte[] seed) throws Exception {
         KeyGenerator kgen = KeyGenerator.getInstance("AES");
-        SecureRandom sr = SecureRandom.getInstance("SHA1PRNG", "Crypto");
+//        SecureRandom sr = SecureRandom.getInstance("SHA1PRNG", "Crypto");
+        SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
         sr.setSeed(seed);
         kgen.init(128, sr); // 192 and 256 bits may not be available
         SecretKey skey = kgen.generateKey();
